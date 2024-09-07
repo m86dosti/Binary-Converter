0beec871b0ce7b125ee6385cb0d1d31a06eb40f8
‌‌‌𝓜𝓸𝓱𝓪𝓶𝓶𝓪𝓭 <150838661+m86dosti@users.noreply.github.com>
2024-09-07 16:16:55 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function convertToBinary() {
     const sentenceInput = document.getElementById('sentence-input');
     const sentence = sentenceInput.value;
-    
     // Convert sentence to binary text
     let binary = '';
     for (let i = 0; i < sentence.length; i++)
